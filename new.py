58ef33b3051a896a9c936221317bc41f1139b916
QuanZhong WU <wuquanzhong@yahoo.com>
2017-10-23 09:23:31 +0800

    入门

--- new.py
+++ new.py
@@ -14,12 +14,15 @@ print id(b)
 int str list set dict tuple
 
 # 序列通用操作
-索引
-切片
-序列相加
-重复
-检查成员
-长度/最大值/最小值
+# 索引
+# 切片
+# 序列相加
+# 重复
+# 检查成员
+# 长度/最大值/最小值
 
 #str字符串
 '' , "" , """ """
+
+a = 'hello python'
+ print len(a)
